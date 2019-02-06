a922887d87e64da9e2acaf9dc14df63d5d99f1fc
Rohin Knight <rohin.knight@gmail.com>
2019-02-06 17:35:47 +1300

    Added tank and tank player controller c++ classes

---
+++ TankBattle/Source/TankBattle/TankPlayerController.cpp
@@ -0,0 +1,24 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TankPlayerController.h"
+
+void ATankPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	auto ControlledTank = GetControlledTank();
+	if (!ControlledTank)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("PlayerController Not Possessing Tank"));
+		
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("PlayerController Possessing %s"), *(ControlledTank->GetName()));
+	}
+}
+
+ATank* ATankPlayerController::GetControlledTank() const
+{
+	return Cast<ATank>(GetPawn());
+}
